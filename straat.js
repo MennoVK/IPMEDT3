1a1e79fec83b04e059bc23e8e7d73eedeb431797
s1136647 <s1136647@student.hsleiden.nl>
2023-01-23 13:43:01 +0100

    koppelen

--- straat.js
+++ straat.js
@@ -1,5 +1,10 @@
 window.onload = () => {
     const smoke = document.getElementById('smoke-id');
+    const lock = document.getElementsByClassName('js--lock')
 
-    console.log(smoke)
+    for (let i=0; i < lock.length; i++) {
+        lock[i].addEventListener('click', function() {
+            console.log('Je kijkt naar het lock systeem');
+        })
+    }
 }
