42481eef891793047890d0a0469be09c705c48e0
Cas1311 <86728367+Cas1311@users.noreply.github.com>
2023-01-24 13:54:03 +0100

    tp + verandering van audio toegevoegd

---
+++ teleport.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+  const position = document.getElementById('js--camera');
+  const kleedkamerDeur = document.getElementsByClassName('js--door');
+  const housedoor = document.getElementsByClassName('js--housedoor');
+  const truck = document.getElementsByClassName('js--truck')
+  const fire = document.getElementsByClassName('fire');
+
+    for (let i = 0; i < kleedkamerDeur.length; i++) {
+      kleedkamerDeur[i].onmouseenter = (event) => {
+        position.setAttribute("position", "10 1.63 -20");
+        kleedkamerDeur[i].setAttribute('sound', "volume: 0")
+        for (let i = 0; i < truck.length; i++) {
+          truck[i].setAttribute('sound', "volume: 1")
+          }
+      };
+    }
+
+    for (let i = 0; i < housedoor.length; i++) {
+      housedoor[i].onmouseenter = (event) => {
+      position.setAttribute("position", "0 1.63 0");
+      
+      for (let i = 0; i < truck.length; i++) {
+        truck[i].setAttribute('sound', "volume: 0")
+        }
+        
+        for (let i = 0; i < fire.length; i++) {
+          fire[i].setAttribute('sound', "volume: 1")
+          }
+      };
+
+
+    }
+  };
+  
